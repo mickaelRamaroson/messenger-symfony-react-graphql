a30c8a5b44dbf2c8840df0a8f48307e4891bbe17
MickaelRama50 <mickael@mister-e.fr>
2023-02-11 08:01:47 +0300

    disable deprecated

--- backend/public/index.php
+++ backend/public/index.php
@@ -2,9 +2,9 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
+    error_reporting(E_ALL ^ E_DEPRECATED);
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
-
